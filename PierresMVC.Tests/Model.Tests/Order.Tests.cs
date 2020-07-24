48a2c92a987143ebdf595dac477e5f52d1128e20
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-24 13:20:39 -0700

    fail test for NEW order description.

--- PierresMVC.Tests/Model.Tests/Order.Tests.cs
+++ PierresMVC.Tests/Model.Tests/Order.Tests.cs
@@ -22,6 +22,21 @@ namespace PierresMVC.Tests
       string result = newOrder.Description;
       Assert.AreEqual(description, result);
     }
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Local SE Bakery, coffee + light";
+      Order newOrder = new Order(description);
+
+      //Act
+      string updatedDescription = "Local SE Bakery, coffee + light fare for families";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
 
   }
 }

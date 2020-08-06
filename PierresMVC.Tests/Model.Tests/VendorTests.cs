519c604a2d44608bf70bda7faba68cf373ef62ba
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-06 00:25:49 -0700

    pass test for creating object of vendor.

--- PierresMVC.Tests/Model.Tests/VendorTests.cs
+++ PierresMVC.Tests/Model.Tests/VendorTests.cs
@@ -1,4 +1,18 @@
-// using PierresMVC.Models;
-// {
-  
-// }
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresMVC.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresMVC.Tests
+{
+  [TestClass]
+  public class VendorTest
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstancesOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
--- PierresMVC/Models/Vendor.cs
+++ PierresMVC/Models/Vendor.cs
@@ -4,6 +4,12 @@ namespace PierresMVC.Models
 {
   public class Vendor
   {
+    public int VendorId { get; }
+    public string VendorDesc { get; set; }
 
+    public Vendor(string description)
+    {
+      VendorDesc = description;
+    }
   }
 }
